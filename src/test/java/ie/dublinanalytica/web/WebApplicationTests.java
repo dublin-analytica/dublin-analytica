ac7e61cf46344b2ffab0c2033d4d4f7139168608
Evan Brierton <35705911+evanbrierton@users.noreply.github.com>
2022-03-01 00:38:24 +0000

    Initialise Spring Boot project

---
+++ src/test/java/ie/dublinanalytica/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.dublinanalytica.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
