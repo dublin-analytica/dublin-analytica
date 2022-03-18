59d5a5fda2b9f43f8ba7ce04aa34495883f036a8
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2022-03-18 18:17:10 +0000

    Fix status code fore exceptions

--- src/main/java/ie/dublinanalytica/web/exceptions/DatasetAlreadyExistsException.java
+++ src/main/java/ie/dublinanalytica/web/exceptions/DatasetAlreadyExistsException.java
@@ -1,12 +1,15 @@
 package ie.dublinanalytica.web.exceptions;
 
+import org.springframework.http.HttpStatus;
+
 /**
  * Exception thrown when trying to create a dataset that already exists.
  */
 public class DatasetAlreadyExistsException extends BaseException {
-  public static final String DEFAULT_MESSAGE = "Dataset already exists";
+  public static final String DEFAULT_MESSAGE = "Dataset with this name already exists";
+  public static final HttpStatus DEFAULT_HTTP_STATUS = HttpStatus.CONFLICT;
 
   public DatasetAlreadyExistsException() {
-    super(DEFAULT_MESSAGE);
+    super(DEFAULT_MESSAGE, DEFAULT_HTTP_STATUS);
   }
 }
--- src/main/java/ie/dublinanalytica/web/exceptions/UserNotFoundException.java
+++ src/main/java/ie/dublinanalytica/web/exceptions/UserNotFoundException.java
@@ -1,12 +1,15 @@
 package ie.dublinanalytica.web.exceptions;
 
+import org.springframework.http.HttpStatus;
+
 /**
  * Exception thrown when trying to query a user that does not exist.
  */
 public class UserNotFoundException extends BaseException {
   public static final String DEFAULT_MESSAGE = "User couldn't be found";
+  public static final HttpStatus DEFAULT_HTTP_STATUS = HttpStatus.UNAUTHORIZED;
 
   public UserNotFoundException() {
-    super(DEFAULT_MESSAGE);
+    super(DEFAULT_MESSAGE, DEFAULT_HTTP_STATUS);
   }
 }
