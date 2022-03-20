5bf24ba9d07a40ad46ed7d8d6a3702c53799a2cb
cameronkenny101 <cam.e.kenny@gmail.com>
2022-03-20 15:31:56 +0000

    Added new exception for unsafe password

--- src/main/java/ie/dublinanalytica/web/exceptions/PasswordNotStrongException.java
+++ src/main/java/ie/dublinanalytica/web/exceptions/PasswordNotStrongException.java
@@ -1,15 +1,12 @@
 package ie.dublinanalytica.web.exceptions;
 
-import org.springframework.http.HttpStatus;
-
 /**
  * Exception thrown when password is not strong.
  */
 public class PasswordNotStrongException extends BaseException {
-  public static final String DEFAULT_MESSAGE = "Order with that ID couldn't be found";
-  public static final HttpStatus DEFAULT_HTTP_STATUS = HttpStatus.NOT_FOUND;
+  public static final String DEFAULT_MESSAGE = "Password is not strong";
 
   public PasswordNotStrongException() {
-    super(DEFAULT_MESSAGE, DEFAULT_HTTP_STATUS);
+    super(DEFAULT_MESSAGE);
   }
 }
